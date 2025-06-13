ed4578bc0b25e40db2166ae24ce22c366dee1af7
bernard-o <kaedro.ceballos@gmail.com>
2025-06-13 16:54:00 -0300

    Simplificar

--- test1/addition.cpp
+++ test1/addition.cpp
@@ -2,30 +2,19 @@
 
 using namespace std;
 
-extern "C" int IntegerAddSub_(int a, int b, int c, int d);
-
-void PrintResult(const char* msg, int a, int b, int c, int d, int result) {
-    const char nl = '\n';
-    cout << msg << nl;
-    cout << "a = " << a << nl;
-    cout << "b = " << b << nl;
-    cout << "c = " << c << nl;
-    cout << "d = " << d << nl;
-    cout << "result = " << result << nl;
-    cout << nl;
-}
+extern "C" int IntegerAdd_(int a, int b);
 
 int main(int argc, char** argv) {
     
-    int a, b, c, d, result;
+    int a, b, r;
     
-    a = 10; b = 20; c = 30; d = 18;
-    result = IntegerAddSub_(a, b, c, d);
-    PrintResult("Test case #1", a, b, c, d, result);
+    a = 10; b = 20;;
+    r = IntegerAdd(a, b);
+    cout << a << " + " << b << " = " << r
     
-    a = 101; b = 34; c = -190; d = 25;
-    result = IntegerAddSub_(a, b, c, d);
-    PrintResult("Test case #2", a, b, c, d,result);
+    a = 101; b = 34;
+    r = IntegerAdd(a, b);
+    cout << a << " + " << b << " = " << r
 
     return 0;
 }
