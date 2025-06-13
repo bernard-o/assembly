4326bc95772c45941870f6ce68536d0fdaaf6d40
bernard-o <kaedro.ceballos@gmail.com>
2025-06-13 16:40:20 -0300

    Update addition.s

--- test1/addition.s
+++ test1/addition.s
@@ -1,11 +1,11 @@
 .text
 .global IntegerAddSub_
-  IntegerAddSub_:
+    IntegerAddSub_:
   
-    // Calculate a + b + c - d
+        // Calculate a + b + c - d
 
-    add r0,r0,r1 //r0 = a + b
-    add r0,r0,r2 //r0 = a + b + c
-    sub r0,r0,r3 //r0 = a + b + c - d
+        add r0,r0,r1 //r0 = a + b
+        add r0,r0,r2 //r0 = a + b + c
+        sub r0,r0,r3 //r0 = a + b + c - d
     
-    bx lr //return to caller
+        bx lr //return to caller
