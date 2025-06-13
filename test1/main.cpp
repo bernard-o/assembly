0747e67eb0fd0fa034a5c5c644266ff9d56af9e6
bernard-o <kaedro.ceballos@gmail.com>
2025-06-13 13:44:24 -0300

    Create main.cpp

---
+++ test1/main.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+
+using namespace std;
+
+extern "C" int TestLDR_(void);
+
+int main(int argc, char** argv)
+{
+    int result = TestLDR_();
+    cout << "result = " << result << '\n';
+    return 0;
+}
