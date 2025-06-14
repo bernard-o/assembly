7f178776c653ca2671a26b2b342084ecc72012d9
bernard-o <kaedro.ceballos@gmail.com>
2025-06-14 16:04:32 -0300

    Adição

--- matematica/add.cpp
+++ matematica/add.cpp
@@ -1,1 +1,21 @@
 
+#include <iostream>
+
+using namespace std;
+
+extern "C" int adicionar_(int i, int j);
+
+int main(int argc, char** argv) {
+    
+    int a, b, r;
+    
+    a = 10; b = 20;;
+    r = adicionar_(a, b);
+    cout << a << " + " << b << " = " << r;
+    
+    a = 101; b = 34;
+    r = adicionar_(a, b);
+    cout << "\n" << a << " + " << b << " = " << r;
+
+    return 0;
+}
---
+++ matematica/add.s
@@ -0,0 +1,8 @@
+.text
+.global adicionar_
+
+    adicionar_:
+
+        add w0, w0, w1 //w0 = i + j
+        ret
+        
