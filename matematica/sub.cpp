84674f76a203f04cb9a3eff64799ee016e9c9500
bernard-o <kaedro.ceballos@gmail.com>
2025-06-14 16:38:37 -0300

    Subtração

---
+++ matematica/sub.cpp
@@ -0,0 +1,21 @@
+
+#include <iostream>
+
+using namespace std;
+
+extern "C" int subtrair_(int i, int j);
+
+int main(int argc, char** argv) {
+    
+    int a, b, r;
+    
+    a = 20; b = 10;;
+    r = subtrair_(a, b);
+    cout << a << " - " << b << " = " << r;
+    
+    a = 101; b = 34;
+    r = subtrair_(a, b);
+    cout << "\n" << a << " - " << b << " = " << r;
+
+    return 0;
+}
---
+++ matematica/sub.s
@@ -0,0 +1,8 @@
+.text
+.global subtrair_
+
+    subtrair_:
+
+        sub w0, w0, w1 //w0 = i - j
+        ret
+        
