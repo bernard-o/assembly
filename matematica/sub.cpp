cc1332f57162da90b425eb88d1d5043d46cd64ec
bernard-o <kaedro.ceballos@gmail.com>
2025-06-15 18:49:38 -0300

    Função de subtrair desmembrada em funções de 32b e 64b

--- matematica/sub.cpp
+++ matematica/sub.cpp
@@ -3,18 +3,19 @@
 
 using namespace std;
 
-extern "C" int subtrair_(int i, int j);
+extern "C" int subtrair32b_(int i, int j);
+extern "C" long subtrair64b_(long i, long j);
 
 int main(int argc, char** argv) {
     
     int a, b, r;
     
     a = 20; b = 10;
-    r = subtrair_(a, b);
+    r = subtrair32b_(a, b);
     cout << a << " - " << b << " = " << r;
     
     a = 101; b = 34;
-    r = subtrair_(a, b);
+    r = subtrair64b_(a, b);
     cout << "\n" << a << " - " << b << " = " << r;
 
     return 0;
--- matematica/sub.s
+++ matematica/sub.s
@@ -1,9 +1,16 @@
 .text
 
-    .global subtrair_ // tratar funcao como funcao global
-
-    subtrair_:
+    // extern "C" int subtrair32b_(int i, int j);
+    .global subtrair32b_
+    subtrair32b_:
 
         sub w0, w0, w1 //w0 = i - j
+        ret
+
+    // extern "C" long subtrair64b_(long i, long j);
+    .global subtrair64b_
+    subtrair64b_:
+
+        sub x0, x0, x1 //x0 = i - j
         ret
         
