5ddc72387fc378b68a68580bbcf61f4ea8b5bce0
bernard-o <kaedro.ceballos@gmail.com>
2025-06-15 06:40:17 -0300

    Funções 32, 64, signed e unsigned em mult

--- matematica/mult.cpp
+++ matematica/mult.cpp
@@ -3,19 +3,26 @@
 
 using namespace std;
 
-extern "C" int multiplicar_(int i, int j);
+extern "C" int multiplicar32b_(int i, int j);
+extern "C" long multiplicar64b_(long i, long j);
+extern "C" long multiplicar64b_signed_(int a, int b);
+extern "C" unsigned long multiplicar64b_unsigned(unsigned int i, unsigned int j);
 
 int main(int argc, char** argv) {
     
-    int a, b, r;
-    
-    a = 10; b = 20;
-    r = multiplicar_(a, b);
+    int a = 10, b = 20, r;
+
+    r = multiplicar32b_(a, b);
     cout << a << " * " << b << " = " << r;
-    
-    a = 101; b = 34;
-    r = multiplicar_(a, b);
-    cout << "\n" << a << " * " << b << " = " << r;
+
+    r = multiplicar64b_(a, b);
+    cout << a << " * " << b << " = " << r;
+
+    r = multiplicar64b_signed_(a, b);
+    cout << a << " * " << b << " = " << r;
+
+    r = multiplicar64b_unsigned(a, b);
+    cout << a << " * " << b << " = " << r;
 
     return 0;
 }
--- matematica/mult.s
+++ matematica/mult.s
@@ -1,9 +1,25 @@
 .text
 
-    .global multiplicar_ // tratar funcao como funcao global
-
-    multiplicar_:
-
-        mul w0, w0, w1 //w0 = i * j
+    // extern "C" int multiplicar32b_(int i, int j);
+    .global multiplicar32b_
+    multiplicar32b_:
+        mul w0, w0, w1 // w0 = i * j (32-bit)
         ret
-        
+
+    // extern "C" long multiplicar64b_(long i, long j);
+    .global multiplicar64b_
+    multiplicar64b_:
+        mul x0, x0, x1 // x0 = i * j (64-bit)
+        ret
+
+    // extern "C" long multiplicar64b_signed_(int a, int b);
+    .global multiplicar64b_signed_
+    multiplicar64b_signed_:
+        smull x0, w0, w1 // x0 = i * j signed 64-bit
+        ret
+
+    // extern "C" unsigned long multiplicar64b_unsigned(unsigned int i, unsigned int j);
+    .global multiplicar64b_unsigned
+    multiplicar64b_unsigned:
+        umull x0, w0, w1 // x0 = i * j unsigned 64-bit
+        ret
