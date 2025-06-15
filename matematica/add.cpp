4a61430a26b1346915e042f1a4ece3eeabea3ce3
bernard-o <kaedro.ceballos@gmail.com>
2025-06-15 18:45:47 -0300

    Função de adição  desmembrada entre 32b e 64b

--- matematica/add.cpp
+++ matematica/add.cpp
@@ -3,18 +3,19 @@
 
 using namespace std;
 
-extern "C" int adicionar_(int i, int j);
+extern "C" int adicionar32b_(int i, int j);
+extern "C" long adicionar64b_(long i, long j);
 
 int main(int argc, char** argv) {
     
     int a, b, r;
     
     a = 10; b = 20;
-    r = adicionar_(a, b);
+    r = adicionar32b_(a, b);
     cout << a << " + " << b << " = " << r;
     
     a = 101; b = 34;
-    r = adicionar_(a, b);
+    r = adicionar64_(a, b);
     cout << "\n" << a << " + " << b << " = " << r;
 
     return 0;
--- matematica/add.s
+++ matematica/add.s
@@ -1,9 +1,16 @@
 .text
 
-    .global adicionar_ // tratar funcao como funcao global
-
-    adicionar_:
+    // extern "C" int adicionar32b_(int i, int j);
+    .global adicionar32b_
+    adicionar32b_:
 
         add w0, w0, w1 //w0 = i + j
+        ret
+
+    // extern "C" long adicionar64b_(long i, long j);
+    .global adicionar64b_
+    adicionar64b_:
+
+        add x0, x0, x1 //x0 = i + j
         ret
         
